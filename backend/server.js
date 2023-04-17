fc1201ffab2d1f390a8c01483847efc3a2f81a56
ganeshdeepEcommerce <bhubohara@gmail.com>
2023-04-17 13:51:50 +0545

    creating real time chat app

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const dotenv=require("dotenv");
+const chats = require("./data/data");
+
+
+const app = express();
+dotenv.config();
+
+
+
+app.get("/",(req,res)=>{
+    res.send("api is running");
+});
+
+app.get("/api/chat",(req,res)=>{
+  
+    res.send(chats);
+});
+
+app.get("/api/chat/:id",(req,res)=>{
+    // console.log(req.params.id);
+
+    const singleChat = chats.find((ch)=> ch._id===req.params.id);
+    res.send(singleChat);
+})
+
+
+const PORT = process.env.PORT ||8000
+
+app.listen(8000, console.log(`server is running on  ` ));
